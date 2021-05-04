228830c8311f3fd86cbc6f79b4bd26ce33e97991
Helmut Januschka <helmut@januschka.com>
2021-05-04 21:28:51 +0200

    init

---
+++ lib/krn/auth/ruby.rb
@@ -0,0 +1,10 @@
+require "krn/auth/ruby/version"
+
+module Krn
+  module Auth
+    module Ruby
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/krn/auth/ruby_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Krn::Auth::Ruby do
+  it "has a version number" do
+    expect(Krn::Auth::Ruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
